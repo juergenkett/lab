05049ecc550094c098c02fa8a20e208b982c1df1
juergenkett <juergenkett@gmx.de>
2022-05-27 20:57:03 +0200

    third commit

---
+++ wikibase-tools/src/main/java/de/dnb/afs/wikibase/App.java
@@ -0,0 +1,13 @@
+package de.dnb.afs.wikibase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
